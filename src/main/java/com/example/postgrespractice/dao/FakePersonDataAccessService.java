32b108f8b59263b3f4d0542a31ca2cc72a593684
Jovan Zuniga <jovartzy@gmail.com>
2022-05-22 04:18:36 -0400

    Removes comment

--- src/main/java/com/example/postgrespractice/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/postgrespractice/dao/FakePersonDataAccessService.java
@@ -8,8 +8,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-// adds a comment 
-
 @Repository("fakeDAO")
 public class FakePersonDataAccessService implements PersonDAO {
 
